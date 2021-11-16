34f4c71c81037949c0cb938c5759c900bee747cd
Yevhen Zaika <evgenij.zajka@privatbank.ua>
2021-11-16 02:18:05 +0200

    add hw7

--- src/com/pb/zayka/hw7/Atelier.java
+++ src/com/pb/zayka/hw7/Atelier.java
@@ -2,6 +2,9 @@ package com.pb.zayka.hw7;
 
 public class Atelier {
     public static void main(String[] args) {
+        System.out.println(Size.XS);
+        System.out.println(Size.XS.getDescription());
+
 
     }
 }
--- src/com/pb/zayka/hw7/Size.java
+++ src/com/pb/zayka/hw7/Size.java
@@ -1,4 +1,33 @@
 package com.pb.zayka.hw7;
 
 public enum Size {
+
+    XXS("Детский размер", 32),
+    XS("Взрослый размер", 34),
+    S("Взрослый размер", 36),
+    M("Взрослый размер", 38),
+    L("Взрослый размер", 40);
+
+    private String description;
+    private int euroSize;
+
+    Size(String description, int euroSize) {
+        this.description = description;
+        this.euroSize = euroSize;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getEuroSize() {
+        return euroSize;
+    }
+
+    @Override
+    public String toString() {
+        return description + '\t'
+                + this.name() + '\t'
+                + euroSize;
+    }
 }
